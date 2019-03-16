5e1e248451212ecb358b0585d042156ec6cc63a0
Phong Luc <luctruongphong13@gmail.com>
2019-03-16 08:47:02 +0700

    Init DemoView

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  constructor(props)
+  {
+     super(props);
+     this.state={
+       input:''
+     }
+  }
+  render() {
+    const {input} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+        <div style={{height:'25vh', marginBottom: '4rem'
+      }} >
+          <input style={{fleX:1, height:'100%' }}  type='text' name='title' value={input} onChange={(event)=>{this.setState({input:event.target.value})}}/>
+        </div>
+          <p>
+            {input}
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/index.css';
+import * as serviceWorker from './serviceWorker';
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
