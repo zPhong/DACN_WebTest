7d8ce65ad285e81651627f9c70bf88025eb6532b
Gia Khang <levogiakhang@gmail.com>
2019-06-03 17:37:07 +0700

    Update test

--- src/TestMath.js
+++ src/TestMath.js
@@ -13,21 +13,31 @@ import {
   calculateTwoVariablesFirstDegreeEquations,
   calculateIntersectionLinearEquationWithCircleEquation,
   calculateIntersectionTwoCircleEquations,
-  calculateSetOfLinearEquationAndQuadraticEquation, getLineFromTwoPoints
+  calculateSetOfLinearEquationAndQuadraticEquation,
+  getLineFromTwoPoints,
+  calculatePerpendicularLineByPointAndLine,
+  calculateIntersectionByLineAndLine
 } from './utils/math/Math2D';
 
 class TestMath extends Component {
   render() {
-    const firstPoint: CoordinateType = { x: -11, y: 6, z: 0 };
+    const firstPoint: CoordinateType = { x: -2, y: 3, z: 0 };
     const secondPoint: CoordinateType = { x: 2, y: 6, z: 0 };
 
     const f1: FirstDegreeEquation = { a: 4, b: 3, c: -23 };
     const f2: FirstDegreeEquation = { a: 3, b: -4, c: -36 };
-    const d: LinearEquation = {
-      coefficientX: 0.5,
-      coefficientY: -1,
-      constantTerm: -5.5
+
+    const d1: LinearEquation = {
+      coefficientX: 1,
+      coefficientY: 0,
+      constantTerm: 0
     };
+    const d2: LinearEquation = {
+      coefficientX: 0,
+      coefficientY: 1,
+      constantTerm: -10
+    };
+
     const c1: CircleEquation = {
       a: 0,
       b: 0,
@@ -38,6 +48,7 @@ class TestMath extends Component {
       b: 4,
       r: Math.sqrt(5)
     };
+
     const q: TwoVariableQuadraticEquation = {
       a: 1,
       b: 1,
@@ -49,7 +60,7 @@ class TestMath extends Component {
     return (
       <div className="container">
         <div className="row">
-          {console.log(getLineFromTwoPoints(firstPoint, secondPoint))}
+          {console.log(calculateIntersectionByLineAndLine(d1, d2))}
         </div>
         <div>{calculateDistanceTwoPoints(firstPoint, secondPoint)}</div>
       </div>
